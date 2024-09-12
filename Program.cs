b98af24492f505bbce6a9e79f00b76986cac9e00
revilo-123 <151610666+revilo-123@users.noreply.github.com>
2024-09-12 13:47:32 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1Slett9999
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kall på metodene som er definert under.
+            //Der hvor det er en return, opprett variabel for å holde på returverdien
+            SkrivHei();
+
+            string returverdi = ReturnerHei();
+            Console.WriteLine(returverdi);
+
+            SkrivNoe("Dette er en test!!!!");
+
+                int sum = Add(5, 3);
+            Console.WriteLine("Summen av tallene er: " + sum);
+
+            AddNoReturn(10, 15);
+
+            Console.ReadLine();//for at consolet ikke skal stenges
+        }
+
+        public static void SkrivHei()
+        {
+            //Skriv kodelinjen som skriver hei ut til consol
+            Console.WriteLine("Hei");
+
+        }
+
+        
+        public static string ReturnerHei()
+        {
+            string bolle = "Hei";
+            return bolle;
+        }
+
+        public static void SkrivNoe(string tekst)
+        {
+            Console.WriteLine(tekst);
+        }
+
+        
+        public static int Add(int tall1, int tall2)
+        {
+            return tall1 + tall2;
+        }
+
+        public static void AddNoReturn(int num1, int num2)
+        {
+            Console.WriteLine(num1+num2);
+        }
+    }
+}
